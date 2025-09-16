18f2d5fd289067fff7c6e6829008e41f850adbd0
alderrylfauza <alderryl30@gmail.com>
2025-09-16 15:48:30 +0700

    Finished Tugas 2 after redo

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         name="Cristiano Ronaldo FunkoPop",
         price=500000,
         description="Limited edition FunkoPop of CR7 in Portugal kit",
-        thumbnail="https://example.com/cr7.jpg",
+        thumbnail="https://i5.walmartimages.com/asr/5ed3bec2-9e53-40ac-897b-0f065a480f1f.1a8e834e36c383de62d9b8483c92cae1.png",
         category="Football Player",
         stock=10,
         is_featured=True
