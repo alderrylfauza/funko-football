2a0146eaddea986815dc96ab8849386830ef8651
alderrylfauza <alderryl30@gmail.com>
2025-09-17 09:41:14 +0700

    changed settings

--- funko_football/settings.py
+++ funko_football/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alderryl-juan-funkofootball.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://alderryl-juan-funkofootball.pbp.cs.ui.ac.id/"
+    "https://alderryl-juan-funkofootball.pbp.cs.ui.ac.id"
 ]
 
 
